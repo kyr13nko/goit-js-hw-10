59d27e263a3078084920cf3b385e476d5f507429
Hryhorii Kyriienko <hrhrkrnk@gmail.com>
2023-07-28 15:47:37 +0300

    task 3.1

--- src/js/index.js
+++ src/js/index.js
@@ -1,5 +1,9 @@
 import { fetchBreeds, fetchCatByBreed } from './cat-api';
 
+import { Notify } from 'notiflix/build/notiflix-notify-aio';
+import SlimSelect from 'slim-select';
+import 'slim-select/dist/slimselect.css';
+
 const refs = {
   select: document.querySelector('.breed-select'),
   loader: document.querySelector('.loader'),
@@ -20,7 +24,8 @@ function insertSelectValues() {
       refs.select.classList.remove('is-hidden');
     })
     .catch(() => {
-      refs.error.classList.remove('is-hidden');
+      // refs.error.classList.remove('is-hidden');
+      Notify.failure('Oops! Something went wrong! Try reloading the page!');
     })
     .finally(() => {
       refs.loader.classList.add('is-hidden');
@@ -38,7 +43,8 @@ function onSelectChange(event) {
       createCatInfoMarkup(data);
     })
     .catch(() => {
-      refs.error.classList.remove('is-hidden');
+      // refs.error.classList.remove('is-hidden');
+      Notify.failure('Oops! Something went wrong! Try reloading the page!');
     })
     .finally(() => {
       refs.loader.classList.add('is-hidden');
@@ -53,14 +59,23 @@ function createSelectMarkup(arr) {
     .join('');
 
   refs.select.insertAdjacentHTML('beforeend', selectValues);
+
+  new SlimSelect({
+    select: '#single',
+    settings: {
+      placeholderText: 'Please, choose a cat',
+    },
+  });
 }
 
 function createCatInfoMarkup({ breeds, url }) {
   const selectInfo = `
-    <img src="${url}" alt="${breeds[0].name}" width=300>
-    <h2>Breed: ${breeds[0].name}</h2>
-    <p>About: ${breeds[0].description}</p>
-    <p>Temperaments: ${breeds[0].temperament}</p>
+    <img src="${url}" alt="${breeds[0].name}" width="300">
+    <div>
+      <h2>${breeds[0].name}</h2>
+      <p>${breeds[0].description}</p>
+      <p><b>Temperaments:</b> ${breeds[0].temperament}</p>
+    </div>
   `;
 
   refs.info.insertAdjacentHTML('beforeend', selectInfo);
